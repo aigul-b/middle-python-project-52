b795e6890445c40fa663e6f494147a4f4d57da0a
aigul-b <aygulbashirova26@gmail.com>
2026-06-09 20:58:26 +0200

    step 2

--- hexlet-code/task_manager/views.py
+++ hexlet-code/task_manager/views.py
@@ -1,4 +1,7 @@
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def index(request):
-    return HttpResponse("Привет от Хекслет!")
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
